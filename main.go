0de68ecfad3f48f5ba0ce8c6044aa74b1c96b19b
guskoro <guskoro@gmail.com>
2019-04-04 14:43:06 +0700

    Newest

--- main.go
+++ main.go
@@ -2,6 +2,31 @@ package main
 
 import "fmt"
 
+var w string
+
 func main() {
 	fmt.Println("Hello World !")
+	fmt.Println("I want to create new function")
+
+	foo()
+	
+	fmt.Println("I want to show prime number")
+
+	w = "Ditanyain, ditaruk, dimakan, diminum, ditampar"
+
+	for i:= 0; i<=10; i++ {
+		if i%2 == 1 || i == 2 {
+			fmt.Println(i)
+		}
+	}
+
+	boo()
+}
+
+func foo ()  {
+	fmt.Println("Foo is wakeup after your call")
+}
+
+func boo() {
+	fmt.Println("Bye lur")
 }
